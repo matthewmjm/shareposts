b41657d5747d398ab4fb261f7c90f52e7c54020d
matthewmjm <matthewmjm@yahoo.com>
2021-06-11 17:47:20 -0600

    Bootstrap file and core class

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,8 @@
-this is bootstrap
+<?php
+    // Load Libraries
+    require_once 'libraries/Core.php';
+    require_once 'libraries/Controller.php';
+    require_once 'libraries/Database.php';
+
+
+?>
