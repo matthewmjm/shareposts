5416e5e93c91055b2b410efb0677e18b547665e5
matthewmjm <matthewmjm@yahoo.com>
2021-06-11 15:25:25 -0600

    updated sandbox for set and get magic methods

--- phpsandbox/oop.php
+++ phpsandbox/oop.php
@@ -1,11 +1,12 @@
 <?php
-    class Person{
+    class Person {
         private $name;
         private $email;
         // public static $ageLimit = 40;
         private static $ageLimit = 40;
 
             // constructor
+            // Runs when an object is instantiated
         public function __construct($name, $email){
             $this->name = $name;
             $this->email = $email;
@@ -13,7 +14,9 @@
             echo __CLASS__ . ' created<br>';
         }
 
-        // deconstructor
+        // destructor
+        // Called when no other references to a certain object
+        // used for cleanup, closing
         public function __destruct(){
             echo __CLASS__ . ' destroyed<br>';
         }
@@ -53,6 +56,58 @@
     // $person1->name = "John Doe";
     // echo $person1->name;
     
+    
+    
+    
+    
+    class User {
+        private $name;
+        private $age;
+        
+        public function __construct($name, $age){
+            $this->name = $name;
+            $this->age = $age;
+        }
+        
+        public function getName(){
+            return $this->name;
+        }
+        
+        public function setName($name){
+            $this->name = $name;
+        }
+        
+        // __get MAGIC METHOD
+        public function __get($property){
+            if(property_exists($this, $property)){
+                return $this->$property;
+            }
+        }
+        
+        // __set MAGIC METHOD
+        public function __set($property, $value){
+            if(property_exists($this, $property)){
+                $this->$property = $value;
+            } 
+            return $this;
+        }
+    }
+    
+    echo $user1->set('name');
+        $user1 = new User('John', 40);
+
+    //$user1->name = 'Jeff';
+    //echo $user1->name;
+
+    // echo $user1->setName('Jeff');
+    // echo $user1->getName();
+
+    $user1->__set('age', 41);
+    echo $user1->__get('age');
+
+
+
+    
     class Customer extends Person{
         private $balance;
 
@@ -69,9 +124,9 @@
         public function getBalance(){
             return $this->balance . '<br>';
         }
-
+        
     }
-
+    
     // $customer1 = new Customer('John Doe', 'jdoe@sol.com', 300);
     // echo $customer1->getBalance();
 
