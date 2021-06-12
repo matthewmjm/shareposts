187dbe4342cddcfa82a391ba8eb2e1dd21f07454
matthewmjm <matthewmjm@yahoo.com>
2021-06-11 23:19:05 -0600

    mapping methods & parameters

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,8 +1,18 @@
 <?php
     class Pages {
         public function __construct() {
-            echo 'Pages loaded';
+
         }
+
+        public function index() {
+            
+        }
+
+        public function about($id){
+            echo 'This is About ' . $id;
+        }
+
     }
+
 
 ?>
