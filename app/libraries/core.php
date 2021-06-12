bbabccff94fdd1526815154ccb0ba10eaf1a2fb0
matthewmjm <matthewmjm@yahoo.com>
2021-06-11 22:22:17 -0600

    loading the controller from the URL

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -6,18 +6,41 @@
     */
 
     class Core {
+        // set default values
         protected $currentController = 'Pages';
         protected $currenMethod = 'index';
         protected $params = [];
 
         public function __construct() {
-            $this->getUrl();
+            // print_r($this->getUrl());
+            $url = $this->getUrl();
+            // Look in controllers for first value
+            if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+                // If exists, sets as controller
+                $this->currentController = ucwords($url[0]);
+                // Unset 0 Index
+                unset($url[0]);
+            }
+
+            // Require the controller
+            require_once '../app/controllers/' . $this->currentController . '.php';
+
+            // Instantiate controller class
+            $this->currentController = new $this->currentController;
         }
 
         public function getUrl() {
-            echo $_GET['url'];
+            // echo $_GET['url'];
+            if(isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
         }
-
     }
 
+    /*
+    Notice: Trying to access array offset on value of type null in /Users/matthewmalecki/Development/php/brad-traversy/www/shareposts/app/libraries/Core.php on line 18
+    */
 ?>
